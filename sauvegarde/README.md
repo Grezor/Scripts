d2cf6328c85c89b92d92a4a75fbb0e5b88c8e142
Geoffrey DUPLESSI <gduplessi@LaGontiere.Local>
2019-06-04 08:29:46 +0200

    error git

---
+++ sauvegarde/README.md
@@ -0,0 +1,75 @@
+<p align="center">
+<img src="https://user-images.githubusercontent.com/38507456/58379582-c13f6700-7fa5-11e9-8a4a-f71c46db23b6.png" alt="bash logo"/>
+</p>
+
+# <tt>#!/bin/bash</tt>
+
+
+## Présentation :
+
+
+## A quoi sa sert ? :
+
+## Technologie : 
+	
+	- .bash
+	- .bat 
+
+## Procedure d'execution :
+
+## Description des fichiers :
+
+
+## Que-est-ce qu'un fichier batch ?
+> Il s'agit de simples fichiers textes contenant des lignes de commande qui sont exécutées séquentiellement, l'une après l'autre. Ces fichiers utilisent l'extension spéciale ".bat" ou ".cmd". Les fichiers de ces types sont reconnus et exécutés via une interface (parfois appelée console) fournie par le système et appelée interpréteur de commandes.
+
+<tt>backup.bat</tt>
+
+- changer votre repertoire : ```"C:\Users\repertoire\Desktop\Scripts\" ```
+
+
+| 		   	|      description     																						|
+|----------	|:---------------------------------------------------------------------------------------------------------:|
+| exit	   	| quitter automatiquement 	   																				|
+| rem      	| commentaire         																						| 
+| xcopy    	| Copiez les fichiers et / ou les répertoires dans un autre dossier											|
+| cls	   	| "nettoyer" la console(supprime tout ce qui est ecrit)       												|
+| color A0  | affiche de la couleur dans la console        																| 
+| @echo off	| permet de cacher toutes les lignes de commandes qui sont effectuées lors de l'execution				    |
+| pause     | laisse ouverte la fenetre
+
+
+# fichier log : 
+
+```
+Fichier enregistre = 14:34:55,35 ==== 	31/05/2019  ======= nom_pc ======= Geoffrey
+
+```
+
+# fichier delete : 
+
+```
+en cours de modification
+
+```
+# New Features!
+
+	- zip 
+	- copier
+	- supprimer {en attente}
+	- remplacer fichier si modifier
+
+
+# dir : <option> :
+	
+
+
+# liens :
+
+- [xcopy_explication](https://ss64.com/nt/xcopy.html)
+- [log.txt](https://www.computerhope.com/issues/ch001679.htm)
+- [move_files](https://www.computerhope.com/movehlp.htm)
+
+## update readme : 
+
+- 31/05/2019 
---
+++ sauvegarde/backup.bat
@@ -0,0 +1,69 @@
+@echo off
+rem cls permet d'effacer l'affichage 
+If exist "C:\Users\gduplessi\Desktop\Scripts\" (
+    color 0a
+    echo le REPERTOIRE existe  
+
+rem ----------- COPIER UN REPERTOIRE  -------------------------------------------
+: copie le repertoire : source destination 
+xcopy C:\Users\gduplessi\Desktop\C:\Users\gduplessi\Desktop\Scripts C:\Users\gduplessi\Desktop\C:\Users\gduplessi\Desktop\Scripts\sauvegarde 
+echo  COPIE DES FICHIERS EN COURS
+rem -------------------------	FIN XCOPIE -------------------------------------------
+pause
+rem --------------- ZIPER UN REPERTOIRE VERS UNE DESTINATION ------------------------------------------
+rem for /f "tokens=3,2,4 delims=/- " %%x in ("%date%") do set d=%%y%%x%%z
+rem set data=%d%
+rem GOTO 
+rem Echo FICHIER EN COURS DE COMPRESSIONS
+rem GOTO 
+rem "C:\Program Files\7-Zip\7z.exe" a -tzip "C:\Users\gduplessi\Desktop\script_bash\sauvegarde\sauvegarde%d%.zip" "C:\Users\gduplessi\Desktop\script_bash\"
+ : -------------------------	FIN ZIP -------------------------------------------
+
+rem ----- enregistre l'heure / date / nom du pc dans le fichier log
+echo Fichier enregistre = %time% ==== 	%date%  ======= %COMPUTERNAME% ======= %USERNAME%>> log.txt
+
+echo LA SAUVEGARDE A BIEN ETAIS ENREGISTRE
+
+pause
+
+
+
+rem --------------- SUPPRIMER FICHIER  ------------------------------------------
+: SET MYFILE="C:\Users\gduplessi\Desktop\script_bash\sauvegarde\sauvegarde%d%.zip"
+: IF EXIST %MYFILE% DEL /F %MYFILE%
+
+: rem supprimme le fichier au bout après x jours 
+: forfiles -p "C:\Users\gduplessi\Desktop\script_bash\" -s -m *.* -d <number of days> -c "cmd /c del @path"
+
+: set chemin= »D:\Desktop\sauvegarde »
+: set jours=1
+
+
+: rem supprime et affiche les dossiers qui ont été supprimé
+
+: ForFiles /P %chemin% /D -%jours% /C « CMD /C if @ISDIR==TRUE echo @FILE &RD /Q /S 
+: @FILE »
+rem --------------- / SUPPRIMER FICHIER  ------------------------------------------
+
+
+pause
+
+
+
+rem Verification si la personne a internet
+rem pause > null
+rem echo checking internet connection
+rem Ping www.google.nl -n 1 -w 1000
+rem if errorlevel 1 (set internet=Not connected to internet)
+rem if errorlevel 0 (set internet=Connected to internet)
+
+
+) else (
+    color 0c
+    echo ERREUR REPERTOIRE
+    echo Veuillez prevenir le service informatique
+  	
+  	
+)
+
+pause
