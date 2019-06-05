41e359f0f02d9daf33a471dc761dea908b85348e
Grezor <grezor.gamer@gmail.com>
2019-06-05 12:19:20 +0200

    terminer, a améliorer

--- test.bat
+++ test.bat
@@ -4,17 +4,18 @@ IF EXIST "C:\Users\gduplessi\Desktop\Scripts" (
 echo repertoire EXIST
 
 echo ==========================
-echo %date%
+echo Nous somme le %date%
 echo ==========================
 
  
 rem ----------- COPIER UN REPERTOIRE  -------------------------------------------
 
 rem xcopy C:\Users\gduplessi\Desktop\Scripts C:\Users\gduplessi\Desktop\Scripts\sauvegarde 
-rem echo  COPIE DES FICHIERS EN COURS
-rem echo Fichier enregistre = %time% ==== 	%date%  ======= %COMPUTERNAME% ======= %USERNAME% >> log.txt
-rem echo TERMINER 
-rem pause
+xcopy "C:\Users\gduplessi\Desktop\Scripts" "C:\Users\gduplessi\Documents\sauvegarde" /e /i /c /h /y
+echo  COPIE DES FICHIERS EN COURS
+
+echo TERMINER 
+
 
 rem ----------- FIN COPIER UN REPERTOIRE  -------------------------------------------
 
@@ -23,18 +24,18 @@ rem ----------- FIN COPIER UN REPERTOIRE  --------------------------------------
 rem -------------------------------------------
 rem 				ZIP  
 rem -------------------------------------------
-rem  for /f "tokens=3,2,4 delims=/- " %%x in ("%date%") do set d=%%y%%x%%z
-rem  set data=%d%
-rem  GOTO 
-rem  Echo FICHIER EN COURS DE COMPRESSIONS
-rem  GOTO 
-rem "C:\Program Files\7-Zip\7z.exe" a -tzip "C:\Users\gduplessi\Desktop\Scripts\sauvegarde\sauvegarde%COMPUTERNAME%.zip" "C:\Users\gduplessi\Desktop\Scripts\"
+ for /f "tokens=3,2,4 delims=/- " %%x in ("%date%") do set d=%%y%%x%%z
+ set data=%d%
+ GOTO 
+ Echo FICHIER EN COURS DE COMPRESSIONS
+ GOTO 
+"C:\Program Files\7-Zip\7z.exe" a -tzip "C:\Users\gduplessi\Documents\sauvegarde\sauvegarde%COMPUTERNAME%.zip" "C:\Users\gduplessi\Desktop\Scripts\"
 rem echo Fichier enregistre = %time% ==== 	%date%  ======= %COMPUTERNAME% ======= %USERNAME% >> C:\Users\gduplessi\Desktop\Scripts\rapport\backup.txt
-rem echo ====================================================
-rem echo ==== la sauvegarde a bien ete enregistre 
-rem echo ====================================================
+echo ====================================================
+echo ==== la sauvegarde a bien ete enregistre 
+echo ====================================================
 
-rem pause
+
 
 rem ----------- FIN ZIP -----------------------------------------
 
@@ -42,52 +43,12 @@ rem ----------- FIN ZIP -----------------------------------------
 
 rem ----------- AFFICHE LISTE DES FICHIERS CREATE / DELETE  -------------------------------------------
 
-rem dir /b /a-d C:\Users\gduplessi\Desktop\Scripts\
-echo ====================================================
-echo Fichier modifier
-echo ====================================================
-
-rem FC diff.txt diff2.txt
-REM /r /Y /I /v /D /q /f /E
-
-
-
 rem attention, pour copier un repertoire, ne pas le mettre dans le meme repertoire que le .bat
-
 rem xcopy "C:\Users\gduplessi\Desktop\Scripts" "C:\Users\gduplessi\Documents\sauvegarde" /e /i /c /h /y
 
 
 
-
-
-rem >>>>>>>>>>>>>>> sauvegarde : 04/06/2019
-for /f "tokens=1-5 delims=/ " %%d in ("%date%") do echo Nous somme le %%d_%%e_%%f
-set tm=%time%
-set hh=!tm:~0,2!
-if !hh! lss 06 (
-echo less    
-) else (
-echo more
-)
-pause
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+rem for /f "tokens=3,*" %%a in ('dir C:\Users\gduplessi\Documents\sauvegarde\*.zip ^|find /i "file" ^|find "/2000 "') do move "%%b" " C:\Users\gduplessi\Documents\sauvegarde"
 
 
 
@@ -107,7 +68,9 @@ echo ====================================================
 echo TERMINER
 echo ====================================================
 
-pause
+pause 
+sleep 10
+exit
 rem ----------- / fin   -------------------------------------------
 
 ) ELSE (
