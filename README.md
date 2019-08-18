cfb75ea5e0224c7ebb1399ed98cab9ecc22f61c7
Grezor <grezor.gamer@gmail.com>
2019-08-18 09:47:03 +0200

    create new folder script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 <img src="https://user-images.githubusercontent.com/38507456/58379582-c13f6700-7fa5-11e9-8a4a-f71c46db23b6.png" alt="bash logo"/>
 </p>
 
-# <tt>#!/bin/bash</tt>
+## !/bin/bash
 [![Github Releases](https://img.shields.io/badge/Update-05.06.2019-blue.svg)]()
 
 ## Présentation :
---
+++ new_script/backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+
+# Creation du nom de l'archive
+create_archive_filename(){
+    day=$(date+ %A)
+    hostname=$(hostname -s)
+    archive_file = "$hostname-$day.tgz"
+}
+
+print_start_status_message(){
+    echo "Backup up $backup_files to $dest/$archive_file"
+    date
+}
+
+# sauvegarder les fichiers en utilisant tar
+backup_files(){
+    tar czf $dest/$archive_file $backup_files
+}
+
+
+print_end_status_message(){
+   
+    echo "backup finished"
+    date
+}
+
+main(){
+    # les dossiers a sauvegarder
+    folder_to_backup ="/home "
+    # la destinations de la sauvegarde 
+    backup_destinations="/destination/backup"
+    create_archive_filename
+    print_start_status_message
+    backup_files
+    print_end_status_message
+    #listing of files in $dest to check file sizes
+    ls -lh $dest
+}
+
+
